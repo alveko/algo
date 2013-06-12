cded825af2ef249d01cc90c4de0ae4b69149f185
Alexander Korobeynikov <alexander.korobeynikov@gmail.com>
2013-06-12 08:18:42 +0200

    cc_lis updated to use hpp implementation + tests

--- cc_lis/lis.cc
+++ cc_lis/lis.cc
@@ -2,119 +2,122 @@
 #include <iterator> 
 #include <algorithm> // std::max()
 #include <iostream>  // std::cin, std::cout
+#include <stdlib.h>  // rand()
+#include <time.h>    // time()
+
+#include <boost/program_options.hpp>
+#include <boost/format.hpp>
 
 #include "algo/longest_increasing_subsequence.hpp"
 
-std::vector<size_t> compute_lis(const std::vector<int> &seq)
+namespace po = boost::program_options;
+
+void print_lis(const std::vector<int> &seq,
+               const std::vector<size_t> &lis)
 {
-    int  maxln = 0;
-    std::vector<int> tails(seq.size()+1, -1);
-    std::vector<int> prevs(seq.size(), -1);
-    
+    // print result (lis elements marked with "+")
+    size_t j = 0;
     for (size_t i = 0; i < seq.size(); i++) {
 
-        // binary search for a tail less than current element
-        int j  = 0;
-        int lo = 1;
-        int hi = maxln;
-        while (lo <= hi) {
-            int k = lo + (hi - lo) / 2;
-            if (seq[tails[k]] < seq[i]) {
-                j = k;
-                lo = k + 1;
-            } else {
-                hi = k - 1;
-            }
-        }
-        
-        prevs[i] = tails[j];
-        if ((j == maxln) || (seq[i] < seq[tails[j+1]])) {
-            tails[j+1] = i;
-            maxln = std::max(maxln, j + 1);
+        if (i == lis[j]) {
+            std::cout << boost::format("%2s%1s ") % seq[i] % "+";
+            j++;
+        } else {
+            std::cout << boost::format("%2s%1s ") % seq[i] % " ";
         }
     }
-
-    // backtrack and store the result
-    std::vector<size_t> lis(maxln, 0);
-    int n = maxln;
-    int i = tails[maxln];
-    while (i != -1 && n > 0) {
-        lis[--n] = i;
-        i = prevs[i];
-    }
-
-    return lis;
+    std::cout << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
+    size_t size = 10;
+    int minval = 0;
+    int maxval = 100;
+    int verbose = 0;
+    
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "Show help")
+        ("verbose,v", po::value<int>(&verbose)->default_value(verbose),
+         "Verbose output level: 0, 1 or 2")
+        ("lis", po::value<std::string>()->default_value("all"),
+         "LIS algorithm:\n<dp | nlogn | all>")
+        ("size", po::value<size_t>(&size)->default_value(size),
+         "Size of the input array for LIS")
+        ("minval", po::value<int>(&minval)->default_value(minval),
+         "Min random value of the array")
+        ("maxval", po::value<int>(&maxval)->default_value(maxval),
+         "Max random value of the array");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help") ||
+        (vm["lis"].as<std::string>() != "dp" &&
+         vm["lis"].as<std::string>() != "nlogn" &&
+         vm["lis"].as<std::string>() != "all")) {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+    
     std::vector<int> seq;
     std::vector<size_t> lis;
+    std::ostream_iterator<int> out_it (std::cout, " ");
 
+    srand(time(NULL));
+    seq.resize(size);
+    std::generate(seq.begin(), seq.end(),
+                  [ minval, maxval ]() -> int
+                  { return minval + rand() % (maxval - minval + 1); });
+
+    if (verbose > 1) {
+        std::cout << boost::format("LIS type: %s\n") % vm["lis"].as<std::string>();
+        std::cout << "seq  = ";
+        std::copy(seq.begin(), seq.end(), out_it);
+        std::cout << std::endl;
+    }
+    
+    /*
     // read input sequence
     std::copy(std::istream_iterator<int>(std::cin), 
               std::istream_iterator<int>(),
               std::back_inserter(seq));
-
-    // compute longest increasing subsequence
-    lis = compute_lis(seq);
-
-    /*
-    seq[0] = 3;
-    std::cout << "seq.size()  = " << seq.size() << std::endl;
-    std::cout << "end - begin = " << seq.end() - seq.begin() << std::endl;
-
-    std::cout << "*begin      = " << *(seq.begin()) << std::endl;
-    std::cout << "*end        = " << *(seq.end()) << std::endl;
-    std::cout << "*(end-1)    = " << *(seq.end()-1) << std::endl;
     */
 
-    size_t lislen1;
-    size_t lislen2;
-    size_t lislen3;
-    std::vector<size_t> lis1;
-    std::vector<size_t> lis2;
-    std::vector<size_t> lis3;
+    // compute longest increasing subsequence
+    std::vector<size_t> lis_dp;
+    std::vector<size_t> lis_nlogn;
 
-    lislen1 = algo::longest_increasing_subsequence(seq.begin(), seq.end());
-    
-    lislen2 = algo::longest_increasing_subsequence(seq.begin(), seq.end(),
-                                                   std::back_inserter(lis2));
-    
-    lislen3 = algo::longest_increasing_subsequence_dp(seq.begin(), seq.end(),
-                                                      std::back_inserter(lis3));
-
-    // print result (lis elements marked with "+")
-    std::cout << "LIS size    = " << lis.size() << std::endl;
-    std::cout << "lislen1     = " << lislen1 << std::endl;
-    std::cout << "lislen2     = " << lislen2 << std::endl;
-    std::cout << "lislen3     = " << lislen3 << std::endl;
-    std::cout << "lis1.size() = " << lis1.size() << std::endl;
-    std::cout << "lis2.size() = " << lis2.size() << std::endl;
-    std::cout << "lis3.size() = " << lis3.size() << std::endl;
-
-    std::ostream_iterator<int> out_it (std::cout, " ");
-    std::cout << "lis  = ";
-    std::copy (lis.begin(), lis.end(), out_it);
-    std::cout << std::endl;
-  
-    std::cout << "lis2 = ";
-    std::copy (lis2.begin(), lis2.end(), out_it);
-    std::cout << std::endl;
-    
-    std::cout << "lis3 = ";
-    std::copy (lis3.begin(), lis3.end(), out_it);
-    std::cout << std::endl;
-    
-    size_t j = 0;
-    for (size_t i = 0; i < seq.size(); i++) {
-        if (i == lis[j]) {
-            std::cout << seq[lis[j++]] << "+ ";
-        } else {
-            std::cout << seq[i] << " ";
-        }
-        
+    if (vm["lis"].as<std::string>() == "dp" ||
+        vm["lis"].as<std::string>() == "all") {
+        algo::longest_increasing_subsequence_dp(seq.begin(), seq.end(),
+                                                std::back_inserter(lis_dp));
     }
-    std::cout << std::endl;
+
+    if (vm["lis"].as<std::string>() == "nlogn" ||
+        vm["lis"].as<std::string>() == "all") {
+        algo::longest_increasing_subsequence(seq.begin(), seq.end(),
+                                             std::back_inserter(lis_nlogn));
+    }
+    
+    if (verbose) {
+        std::cout << "lis_dp.size()    = " << lis_dp.size() << std::endl;
+        std::cout << "lis_nlogn.size() = " << lis_nlogn.size() << std::endl;
+    }
+    
+    if (verbose > 1) {
+        std::cout << "lis_dp     = ";
+        std::copy (lis_dp.begin(), lis_dp.end(), out_it);
+        std::cout << " => ";
+        print_lis(seq, lis_dp);
+        
+        std::cout << "lis_nlogn  = ";
+        std::copy (lis_nlogn.begin(), lis_nlogn.end(), out_it);
+        std::cout << " => ";
+        print_lis(seq, lis_nlogn);
+    }
+
     return 0;
 }
---
+++ cc_lis/run_tests.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('../test_utils')
+import test_utils
+
+def tc01_nlogn():
+    n = 10**8
+    seq = [n/10*i for i in range(1, 11)]
+    cmd = "./lis --lis nlogn --size $x"
+
+    test_utils.run_seq_time("Testing run time:",
+                            seq, cmd, "out_time_nlogn", 1)
+
+    test_utils.run_seq_memo("Testing memory usage:",
+                            seq, cmd, "out_memo_nlogn", 1, "mb")
+
+def tc02_dp():
+    n = 10**5
+    seq = [n/10*i for i in range(1, 11)]
+    cmd = "./lis --lis dp --size $x"
+
+    test_utils.run_seq_time("Testing run time:",
+                            seq, cmd, "out_time_dp", 1)
+
+    test_utils.run_seq_memo("Testing memory usage:",
+                            seq, cmd, "out_memo_dp", 1, "kb")
+
+def run_tests():
+    tc01_nlogn()
+    tc02_dp()
+
+def run_gnuplot():
+    gp = dict(outpng  = "plot_dp.png",
+              title   = "LIS - Dynamic Programming O(N^{2})",
+              xlabel  = "Size of the input sequence",
+              y1label = "Time (sec)",
+              y2label = "Memory (Mb)",
+              title1  = "time",
+              title2  = "memory",
+              file1   = "out_time_dp",
+              file2   = "out_memo_dp",
+              y2rmin  = "*",
+              y2rmax  = "10",
+              y2factor = "0.001")
+    test_utils.gnuplot_1x2y(**gp)
+    
+    gp = dict(outpng  = "plot_nlogn.png",
+              title   = "LIS - Algorithm O(N log(N))^{}",
+              xlabel  = "Size of the input sequence",
+              y1label = "Time (sec)",
+              y2label = "Memory (Mb)",
+              title1  = "time",
+              title2  = "memory",
+              file1   = "out_time_nlogn",
+              file2   = "out_memo_nlogn")
+    test_utils.gnuplot_1x2y(**gp)
+
+def main():
+    run_tests()
+    run_gnuplot()
+
+if __name__ == "__main__":
+    main()
